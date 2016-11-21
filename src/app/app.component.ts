3e0542379332fa70bb6fc82ff10d9179805b43e4
evansmwendwa <evansmwendwa@live.com>
2016-11-21 15:22:17 +0300

    added sample app for testing daterangepicker component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DaterangepickerConfig } from './daterangepicker';
+
 import '../../public/css/styles.css';
+import * as moment from 'moment';
+
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent implements OnInit{
+
+    public dateInputs: any = [
+        {
+            start: moment().subtract(12, 'month'),
+            end: moment().subtract(6, 'month')
+        },
+        {
+            start: moment().subtract(9, 'month'),
+            end: moment().subtract(6, 'month')
+        },
+        {
+            start: moment().subtract(4, 'month'),
+            end: moment()
+        },
+        {
+            start: moment(),
+            end: moment().add(5, 'month'),
+        }
+    ];
+
+    constructor(private daterangepickerOptions: DaterangepickerConfig) {
+        this.daterangepickerOptions.settings = {
+            locale: { format: 'YYYY-MM-DD' },
+            alwaysShowCalendars: false,
+            ranges: {
+               'Last Month': [moment().subtract(1, 'month'), moment()],
+               'Last 3 Months': [moment().subtract(4, 'month'), moment()],
+               'Last 6 Months': [moment().subtract(6, 'month'), moment()],
+               'Last 12 Months': [moment().subtract(12, 'month'), moment()],
+            }
+        };
+    }
+
+    ngOnInit() {}
+
+    private selectedDate(value: any, dateInput: any) {
+        dateInput.start = value.start;
+        dateInput.end = value.end;
+    }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }   from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { Daterangepicker } from './daterangepicker';
+
 @NgModule({
-  imports: [
-    BrowserModule
-  ],
-  declarations: [
-    AppComponent
-  ],
-  bootstrap: [ AppComponent ]
+    imports: [
+        BrowserModule,
+        FormsModule,
+        Daterangepicker
+    ],
+    declarations: [
+        AppComponent
+    ],
+    bootstrap: [ AppComponent ]
 })
 export class AppModule { }
