e0f0af5fd77ff9fc6d021cebae1da82909c3474f
evansmwendwa <evansmwendwa@live.com>
2016-11-21 15:30:32 +0300

    updated package entry point

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./src/app/daterangepicker/daterangepicker.module"));
+__export(require("./src/app/daterangepicker/daterangepicker.component"));
+__export(require("./src/app/daterangepicker/config.service"));
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,3 @@
+export * from './src/app/daterangepicker/daterangepicker.module';
+export * from './src/app/daterangepicker/daterangepicker.component';
+export * from './src/app/daterangepicker/config.service';
