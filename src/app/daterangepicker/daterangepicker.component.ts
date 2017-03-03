1bc4437d6d05a662b63a9fca6bd97bfc19711b17
evansmwendwa <evansmwendwa@live.com>
2017-03-03 14:10:00 +0300

    added support for datepicker events

--- src/app/daterangepicker/daterangepicker.component.ts
+++ src/app/daterangepicker/daterangepicker.component.ts
@@ -13,6 +13,14 @@ export class DaterangePickerComponent implements AfterViewInit {
     @Input() options: any = {};
     @Output() selected = new EventEmitter();
 
+    // daterangepicker events
+    @Output() cancelDaterangepicker = new EventEmitter();
+    @Output() applyDaterangepicker = new EventEmitter();
+    @Output() hideCalendarDaterangepicker = new EventEmitter();
+    @Output() showCalendarDaterangepicker = new EventEmitter();
+    @Output() hideDaterangepicker = new EventEmitter();
+    @Output() showDaterangepicker = new EventEmitter();
+
     constructor(private input: ElementRef, private config: DaterangepickerConfig) { }
 
     ngAfterViewInit() {
@@ -21,6 +29,48 @@ export class DaterangePickerComponent implements AfterViewInit {
         let targetOptions: any = Object.assign({}, this.config.settings, this.options);
 
         $(this.input.nativeElement).daterangepicker(targetOptions, this.callback.bind(this));
+
+        $(this.input.nativeElement).on('cancel.daterangepicker',
+            (e:any, picker:any) => {
+                let event = { event: e, picker: picker };
+                this.cancelDaterangepicker.emit(event);
+            }
+        );
+
+        $(this.input.nativeElement).on('apply.daterangepicker',
+            (e:any, picker:any) => {
+                let event = { event: e, picker: picker };
+                this.applyDaterangepicker.emit(event);
+            }
+        );
+
+        $(this.input.nativeElement).on('hideCalendar.daterangepicker',
+            (e:any, picker:any) => {
+                let event = { event: e, picker: picker };
+                this.hideCalendarDaterangepicker.emit(event);
+            }
+        );
+
+        $(this.input.nativeElement).on('showCalendar.daterangepicker',
+            (e:any, picker:any) => {
+                let event = { event: e, picker: picker };
+                this.showCalendarDaterangepicker.emit(event);
+            }
+        );
+
+        $(this.input.nativeElement).on('hide.daterangepicker',
+            (e:any, picker:any) => {
+                let event = { event: e, picker: picker };
+                this.hideDaterangepicker.emit(event);
+            }
+        );
+
+        $(this.input.nativeElement).on('show.daterangepicker',
+            (e:any, picker:any) => {
+                let event = { event: e, picker: picker };
+                this.showDaterangepicker.emit(event);
+            }
+        );
     }
 
     private callback(start?: any, end?: any): void {
