1e03ca63d3b959d298e7e1a73d0169359e5e2d41
evansmwendwa <evansmwendwa@live.com>
2017-03-09 16:19:10 +0300

    fixed duplicated css styles when using multiple calendars

--- src/app/daterangepicker/daterangepicker.component.ts
+++ src/app/daterangepicker/daterangepicker.component.ts
@@ -2,8 +2,6 @@ import { Directive, OnInit, AfterViewInit, Input, Output, EventEmitter, ElementR
 import { ControlValueAccessor } from '@angular/forms';
 import { DaterangepickerConfig } from './config.service';
 
-declare var require:any;
-
 import * as $ from "jquery";
 import * as moment from 'moment';
 import 'bootstrap-daterangepicker';
@@ -27,9 +25,11 @@ export class DaterangePickerComponent implements AfterViewInit {
     constructor(private input: ElementRef, private config: DaterangepickerConfig) { }
 
     ngAfterViewInit() {
-        $('head').append('<style>'+ require('./daterangepicker.css')+'</style>');
 
         let targetOptions: any = Object.assign({}, this.config.settings, this.options);
+
+        // tell config service to embed the css
+        this.config.embedCSS();
 
         // cast $ to any to avoid jquery type checking
         //$(this.input.nativeElement).daterangepicker(targetOptions, this.callback.bind(this));
