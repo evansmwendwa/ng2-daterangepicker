10897e6defb4baea995cc164a726b0bb8e4febb0
slidenergy <slidenergy@gmail.com>
2018-09-25 12:33:03 +1000

    Add forRoot method for module class

--- src/app/daterangepicker/daterangepicker.module.ts
+++ src/app/daterangepicker/daterangepicker.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 //import { FormsModule }   from '@angular/forms';
 import { DaterangePickerComponent } from './daterangepicker.component';
 import { DaterangepickerConfig } from './config.service';
@@ -6,8 +6,16 @@ import { DaterangepickerConfig } from './config.service';
 @NgModule({
     //imports: [FormsModule],
     declarations: [DaterangePickerComponent],
-    providers: [DaterangepickerConfig],
     exports: [DaterangePickerComponent]
 
 })
-export class Daterangepicker { }
+export class Daterangepicker {
+    public static forRoot(): ModuleWithProviders {
+        return {
+            ngModule: Daterangepicker,
+            providers: [
+                DaterangepickerConfig
+            ]
+        }
+    }
+}
